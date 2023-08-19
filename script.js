cf74f0bb388b660a81f44762cbed0864fd270314
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-18 21:54:09 -0500

    commiting changes

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ function onReady() {
 
 }
 
-function pushTheInput() {
+// The Function That pushes the text into the box below
+function pushTheInput(event) {
+    event.preventDefault();
     console.log('is it working');
-    $('#tableBody').append('<tr><td>hi</td><td>hi</td><td>hi</td><td>hi</td><td>hi</td><<td>hi</td>/tr>')
+    //Set Values To Each Box so we can push
+    let firstName = $('#firstName').val();
+    let lasName = $('#lastName').val();
+    $('#tableBody').append(`<tr><td>${firstName}</td><td>hi</td><td>hi</td><td>hi</td><td>hi</td><<td><button class "deleteButton">Delete</button></td>/tr>`)
 }
